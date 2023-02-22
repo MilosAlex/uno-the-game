003a7372a63f9b946feac4cc9140bb69a1fb645e
Milos Alex <dw5pxw@inf.elte.hu>
2023-02-23 00:48:18 +0100

    feat: updated add room api with deck handling

--- pages/api/addRoom.ts
+++ pages/api/addRoom.ts
@@ -2,6 +2,26 @@ import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
 
 export default async (req: any, res: any) => {
+  function shuffle(array: any[]) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
   try {
     const client = await clientPromise;
     const db = client.db("unodb");
@@ -12,9 +32,20 @@ export default async (req: any, res: any) => {
       return;
     }
 
+    const deck = await db.collection("decks").findOne({ name: "original-uno" });
+
+    if (!deck) {
+      res.status(400).json({ message: "Missing deck" });
+      return;
+    }
+
+    const shuffledDeck = shuffle(deck.cards);
+
     const room = await db.collection("rooms").insertOne({
       hostId: new ObjectId(hostId),
       name,
+      deck: shuffledDeck,
+      round: -1,
     });
 
     res.json(room);
